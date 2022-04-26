5b728f0b8a015bd2b5fe4077e674b0a588cae501
«Vova <vrobskiy@gmail.com>
2022-04-26 14:22:49 +0300

    add example 004

---
+++ 004/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Введите количество элементов массива -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[number];
+
+
+
+FillArray(numbers);
+WriteArray(numbers);
+int resultA = Kolvo(numbers);
+Console.WriteLine(resultA);
+
+
+void FillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(0, 124);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int Kolvo(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= 99 && array[i] >= 10)
+        {
+            result++;
+        }
+    }
+    return result;
+}
---
+++ 005/Program.cs
@@ -0,0 +1,45 @@
+﻿void Fillrray(int[] array)   
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+    array[i] = new Random().Next(1, 10);
+    }
+}
+void WriteArray(int[] array) 
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[sizeArray];
+
+Fillrray(array);
+WriteArray(array);
+
+int newSizeArray = 0;
+if (sizeArray % 2 == 0)
+{
+    newSizeArray = sizeArray / 2;
+}
+else
+{
+    newSizeArray = (sizeArray / 2) + 1;
+}
+
+int[] newArray = new int[newSizeArray];
+for(int i = 0; i < newArray.Length; i++)
+{
+    newArray[i] = array[i] * array[array.Length - (1 + i)];
+}
+
+if (sizeArray % 2 != 0)
+{
+    newArray[newArray.Length - 1] = array[newArray.Length -1];
+}
+
+WriteArray(newArray);
